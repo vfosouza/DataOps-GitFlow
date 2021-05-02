a938dd136635006d03a7679661d1c2b6ee88a7f9
Logon Aluno <logonaluno@local.com>
2021-05-02 16:37:00 -0300

    arquivos atualizados

--- Build/Job_RAW/HAR/HAR_run.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/talendcsv.jar;../lib/crypto-utils.jar;../lib/talend_file_enhanced_20070724.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;har_0_1.jar;' data_ops.har_0_1.HAR  --context=Default $args
